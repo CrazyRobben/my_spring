f9acdd8f0b674edca38c4310fb5e0c4875d3ae56
CrazyRobben <1115440210@qq.com>
2018-03-12 09:15:50 +0800

    上次提交失败重新提交

--- src/main/java/com/iflytek/BeanDefinition.java
+++ src/main/java/com/iflytek/BeanDefinition.java
@@ -1,15 +1,37 @@
+package com.iflytek;
+
 public class BeanDefinition {
     private Object bean;
+    private String beanClassName;
+    private Class beanClass;
+
+    public BeanDefinition() {
+    }
 
     public BeanDefinition(Object bean) {
         this.bean = bean;
     }
-
     public Object getBean() {
         return bean;
     }
 
     public void setBean(Object bean) {
         this.bean = bean;
+    }
+
+    public String getBeanClassName() {
+        return beanClassName;
+    }
+
+    public void setBeanClassName(String beanClassName) {
+        this.beanClassName = beanClassName;
+    }
+
+    public Class getBeanClass() {
+        return beanClass;
+    }
+
+    public void setBeanClass(Class beanClass) {
+        this.beanClass = beanClass;
     }
 }
--- src/main/java/com/iflytek/beanfactory/AbstractBeanFactory.java
+++ src/main/java/com/iflytek/beanfactory/AbstractBeanFactory.java
@@ -1,26 +1,51 @@
+package com.iflytek.beanfactory;
+
+import com.iflytek.BeanDefinition;
+import com.iflytek.util.CommonUtils;
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class AbstractBeanFactory {
+public abstract class AbstractBeanFactory implements BeanFactory {
     private final Map<String, BeanDefinition> beanDefinitionMap = new HashMap<>();
 
     /**
      * 获取bean
+     *
      * @param name bean name
      * @return
      */
+    @Override
     public Object getBean(String name) {
         BeanDefinition beanDefinition = beanDefinitionMap.get(name);
         return null == beanDefinition ? null : beanDefinition.getBean();
     }
 
     /**
-     *注册一个bean
-     * @param name bean的name
-     * @param object bean
+     * 注册bean
+     *
+     * @param name
+     * @param beanDefinition
      */
-    public void registerBean(String name, Object object) {
-        name = null == name ? object.getClass().getName() : name;
-        beanDefinitionMap.put(name, new BeanDefinition(object));
+    @Override
+    public void registerBeanDefinition(String name, BeanDefinition beanDefinition) {
+        if (null != beanDefinition) {
+            if(null==beanDefinition.getBean()){
+                Object o = doCreateBean(beanDefinition);
+                beanDefinition.setBean(o);
+            }
+            if (null == name) {
+                name = CommonUtils.getDefaultBeanName(beanDefinition);
+            }
+            beanDefinitionMap.put(name, beanDefinition);
+        }
     }
+
+    /**
+     * 初始化bean
+     *
+     * @param beanDefinition
+     * @return
+     */
+    protected abstract Object doCreateBean(BeanDefinition beanDefinition) ;
 }
